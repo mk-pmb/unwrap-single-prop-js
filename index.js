101c94819e97b755cc4a3ff217edc4f9faf3641b
mk-pmb <mk-pmb@users.noreply.github.com>
2025-05-06 20:17:22 +0200

    v0.1.0: Works for me.

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+(function compile() {
+  function len1(x) { return x && (x.length === 1); }
+  const u = function unpackSingleKey(k, x) {
+    if (!x) { return x; }
+    if (k === 0) { return (len1(x) ? x[0] : x); }
+    const v = x[k];
+    const l = (v !== undefined) && Object.keys(x);
+    return (len1(l) && (l[0] === k)) ? v : x;
+  };
+  u.len1 = len1;
+  u.df = u.bind(null, 'default');
+  u.default = u; // Convenience for callers suffering from the u.df case.
+  module.exports = u;
+}());
---
+++ test.usage.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+const eq = require('assert').strictEqual;
+const unwrap = require('./index.js'); // require('unwrap-single-prop')
+
+const toast = { bacon: 3, eggs: 1, cheese: 1 };
+const lunch = [toast];
+const dinner = [toast, 'cookies'];
+const menu = { default: lunch };
+
+let k = 'eggs';
+eq(unwrap(k), undefined);
+eq(unwrap(k, toast),   toast);
+eq(unwrap(k, lunch),   lunch);
+eq(unwrap(k, dinner),  dinner);
+eq(unwrap(k, menu),    menu);
+
+k = '0';
+eq(unwrap(k), undefined);
+eq(unwrap(k, toast),   toast);
+eq(unwrap(k, lunch),   toast);
+eq(unwrap(k, dinner),  dinner);
+eq(unwrap(k, menu),    menu);
+
+k = 0;
+eq(unwrap(k), undefined);
+eq(unwrap(k, toast),   toast);
+eq(unwrap(k, lunch),   toast);
+eq(unwrap(k, dinner),  dinner);
+eq(unwrap(k, menu),    menu);
+
+
+
+const symbols = { undefined: '(-)' };
+
+k = undefined;
+eq(unwrap(k, toast),   toast);
+eq(unwrap(k, lunch),   lunch);
+eq(unwrap(k, dinner),  dinner);
+eq(unwrap(k, menu),    menu);
+eq(unwrap(k, symbols), symbols);
+
+k = String(k);
+eq(k, 'undefined'); // Runtime sanity check
+eq(unwrap(k, toast),   toast);
+eq(unwrap(k, lunch),   lunch);
+eq(unwrap(k, dinner),  dinner);
+eq(unwrap(k, menu),    menu);
+eq(unwrap(k, symbols), '(-)');
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.info('+OK usage tests passed.');
